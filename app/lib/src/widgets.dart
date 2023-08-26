dcb0f7104b66c4263756b58d3a3e8e5ebca864d8
Bruno <brunokoelher@hotmail.com>
2023-08-26 12:15:42 -0300

    feat+refactor: reestruturação do projeto, adição de projeto de biblioteca compartilhada "shared" e projeto do backend.

--- app/lib/src/widgets.dart
+++ app/lib/src/widgets.dart
@@ -1,4 +1,4 @@
-export "widgets/calendar.dart";
+export 'widgets/calendar.dart';
 export "widgets/donations.dart";
 export "widgets/social_networks.dart";
 export "widgets/social_network_tile.dart";
