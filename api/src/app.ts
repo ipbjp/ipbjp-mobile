f8e580f1bab9d5b61ea8dc17f92fb4d6efb9ba40
Paulo Henrique Assis <pieit@live.com>
2023-12-08 13:41:54 -0300

    Update package dependencies

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import * as express from "express";
+import express from "express";
 import * as bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
