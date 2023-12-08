786882ad2237f498e425d8744c42bdf0d3abdabe
Paulo Henrique Assis <pieit@live.com>
2023-12-08 13:50:59 -0300

    Add welcome message and error handling to router

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -4,5 +4,10 @@ import eventRoutes from "./event.routes";
 
 const router = express.Router();
 router.use("/events", eventRoutes);
+router.use("/", (req, res) =>
+  res.status(200).json({ message: "Welcome to IPB JP" })
+);
+
+router.use("*", (req, res) => res.status(404).json({ error: "Not Found" }));
 
 export default router;
